02d1f21c87904a3c689b2962b115a533a5db9b04
Harsh <hkaur@contriverz.com>
2019-09-16 12:40:31 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {GeneralService} from './general.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent {
   title = 'Qr-code';
+  url : string ='';
+  
+
+constructor(private service : GeneralService){
+   this.url = 'http://goggle.com';
+
+
+}
+
+
+
+
+
+
+
+
+
 }
